c228c728351aadc8feff58d828decc0e41a7dc2d
bogdanmatei <matei90bogdan@yahoo.com>
2020-12-08 18:55:16 +0200

    Filter and sort buttons

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import moment from 'moment';
 import '../assets/styles/launch.scss';
+import Select from '../assets/images/select@2x.png';
+import Sort from '../assets/images/sort@2x.png';
 
 const Launch = ({ launches }) => {
   return (
-    <div className='launch-list'>
-      {launches.map((launch, i) => (
-        <div className='launch-item' key={i}>
-          <div className='launch-name'>
-            <p>#{i + 1}</p>
-            <h2>{launch.mission_name}</h2>
+    <>
+      <div className='launch-list'>
+        <div className='filter-btns'>
+          <div>
+            <p>Filter by Year</p>{' '}
+            <span>
+              {' '}
+              <img src={Select} alt='' />{' '}
+            </span>
           </div>
-          <div className='launch-info'>
-            <p>{moment.utc(launch.launch_date_utc).format('Do MMM YYYY')}</p>
-            <h3>{launch.rocket.rocket_name}</h3>
+          <div>
+            <p>Sort Descending</p>{' '}
+            <span>
+              <img src={Sort} alt='' />{' '}
+            </span>
           </div>
         </div>
-      ))}
-    </div>
+        {launches.map((launch, i) => (
+          <div className='launch-item' key={i}>
+            <div className='launch-name'>
+              <p>#{i + 1}</p>
+              <h2>{launch.mission_name}</h2>
+            </div>
+            <div className='launch-info'>
+              <p>{moment.utc(launch.launch_date_utc).format('Do MMM YYYY')}</p>
+              <h3>{launch.rocket.rocket_name}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 

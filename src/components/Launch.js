d461ef2ab6d97d8143804ddcf628398328707d31
bogdanmatei <matei90bogdan@yahoo.com>
2020-12-08 16:11:21 +0200

    Axios, useEffect and useState

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Launch = () => {
-  return <div>Launch</div>;
+const Launch = ({ launches }) => {
+  return (
+    <div>
+      <ul>
+        <li>
+          {launches &&
+            launches.map((launch, i) => {
+              return <h2>launch.links.name</h2>;
+            })}
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Launch;
--- src/screens/LaunchesScreen.js
+++ src/screens/LaunchesScreen.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Launch from '../components/Launch';
 import LaunchHome from '../assets/images/launch-home@3x.png';
 import '../assets/styles/launches.scss';
 
 const LaunchesScreen = () => {
+  const [launches, setLaunches] = useState([]);
+
+  const API = 'https://api.spacexdata.com/v4/launches/latest';
+
+  useEffect(() => {
+    axios
+      .get(API)
+      .then((res) => {
+        console.log(res);
+        setLaunches(res.data);
+      })
+      .catch((error) => console.log(error.message));
+  }, []);
+
   return (
     <>
       <section className='launches'>
         <div className='launches-info'>
           <img src={LaunchHome} alt='Rocket lauching' />
-          <Launch />
+          <Launch launches={launches} />
         </div>
       </section>
     </>
